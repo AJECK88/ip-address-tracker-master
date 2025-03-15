dbcaa093f0ca1a7a10610b5b87937ad4d6ad726a
AJECK88 <eajeckekizee@gmail.com>
2025-03-15 12:47:53 +0100

    the UX and UI view

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const map = document.querySelector('#map').innerHTML;
+async function IPG() {
+  const apiKey = "57825d6b88524a2c972b69eacb02d4c0"; // Replace with your ipgeolocation API key
+  const ip = "192.212.174.101"; // Example IP address
+  const url = `https://api.ipgeolocation.io/ipgeo?apiKey=${apiKey}&ip=${ip}`;
+  try {
+      const response = await fetch(url);
+      if (!response) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      console.log(data);
+      updateUI(data);
+  } catch (error) {
+     
+  }
+}
+IPG();
